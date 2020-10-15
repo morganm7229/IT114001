b89f2cc63b8a39bd8e983631857c5746ff768244
robm7229 <rm833@njit.edu>
2020-10-14 23:16:07 -0400

    Part 4

--- Basics/src/basics/SocketClient.java
+++ Basics/src/basics/SocketClient.java
@@ -1,98 +1,155 @@
 package basics;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-//part 2
+
+import basics.Debug;
+//Part 4
 public class SocketClient {
-	Socket server;
-	public SocketClient() {
-		
-	}
+	private Socket server;
+	private Thread inputThread;
+	private Thread fromServerThread;
+
 	public void connect(String address, int port) {
 		try {
 			server = new Socket(address, port);
-			System.out.println("Client connected");
+			Debug.log("Client connected");
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	public void start() throws IOException {
-		if(server == null) {
-			return;
-		}
-		System.out.println("Listening for input");
-		try(Scanner si = new Scanner(System.in);
-				PrintWriter out = new PrintWriter(server.getOutputStream(), true);
-				BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));){
-			String line = "";
-			while(true) {
+
+	private void listenForKeyboard(Scanner si, ObjectOutputStream out) {
+		// Thread to listen for keyboard input so main thread isn't blocked
+		inputThread = new Thread() {
+			@Override
+			public void run() {
 				try {
-					System.out.println("Waiting for input");
-					line = si.nextLine();
-					if(!"quit".equalsIgnoreCase(line)) {
-						out.println(line);
+
+					while (!server.isClosed()) {
+						Debug.log("Waiting for input");
+						String line = si.nextLine();// this line causes a problem due to blocking IO when the server
+													// terminates
+						if (!"quit".equalsIgnoreCase(line) && line != null) {
+							// grab line and write it to the stream
+							out.writeObject(line);// technically a String is an object so this works
+						} else {
+							Debug.log("Stopping input thread");
+							// we're quitting so tell server we disconnected so it can broadcast
+							out.writeObject("bye");
+							break;
+						}
+						try {
+							sleep(50);
+						} catch (Exception e) {
+							Debug.log("Problem sleeping thread");
+							e.printStackTrace();
+						}
 					}
-					else {
-						break;
-					}
-					line = "";
-					String fromServer = in.readLine();
-					
-					if(fromServer != null) {
-						System.out.println("Reply from server: " + fromServer);
-					}
-					else {
-						System.out.println("Server disconnected");
-						break;
-					}
-				}
-				catch(IOException e) {
-					System.out.println("Connection dropped");
-					break;
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					close();
+					Debug.log("Stopped listening to console input");
 				}
 			}
-			System.out.println("Exited loop");
+		};
+		inputThread.start();// start the thread
+	}
+
+	private void listenForServerMessage(ObjectInputStream in) {
+		// Thread to listen for responses from server so it doesn't block main thread
+		fromServerThread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					String fromServer;
+					// while we're connected, listen for strings from server
+					while (!server.isClosed() && (fromServer = (String) in.readObject()) != null) {
+						// keep this one as sysout otherwise if we turn of Debug.log we'll not see
+						// messages
+						System.out.println(fromServer);
+					}
+				} catch (Exception e) {
+					if (!server.isClosed()) {
+						e.printStackTrace();
+						Debug.log("Server closed connection");
+					} else {
+						Debug.log("Connection closed");
+					}
+				} finally {
+					close();
+					Debug.log("Stopped listening to server input");
+				}
+			}
+		};
+		fromServerThread.start();// start the thread
+	}
+
+	public void start() throws IOException {
+		if (server == null) {
+			return;
 		}
-		catch(Exception e) {
+		Debug.log("Client Started");
+		// listen to console, server in, and write to server out
+		try (Scanner si = new Scanner(System.in);
+				ObjectOutputStream out = new ObjectOutputStream(server.getOutputStream());
+				ObjectInputStream in = new ObjectInputStream(server.getInputStream());) {
+
+			// starts new thread
+			listenForKeyboard(si, out);
+
+			// starts new thread
+			listenForServerMessage(in);
+
+			// Keep main thread alive until the socket is closed
+			// initialize/do everything before this line
+			// (Without this line the program would stop after the first message
+			while (!server.isClosed()) {
+				Thread.sleep(50);
+			}
+			Debug.log("Exited loop");
+			Debug.log("Press enter to stop the program");
+			// alternatively in this case we could nuke the program with
+			// System.exit(0);
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			close();
 		}
 	}
+
 	private void close() {
-		if(server != null) {
+		if (server != null && !server.isClosed()) {
 			try {
 				server.close();
-				System.out.println("Closed socket");
+				Debug.log("Closed socket");
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 		SocketClient client = new SocketClient();
 		int port = -1;
-		try{
-			//not safe but try-catch will get it
+		try {
+			// not safe but try-catch will get it
 			port = Integer.parseInt(args[0]);
+		} catch (Exception e) {
+			Debug.log("Invalid port");
 		}
-		catch(Exception e){
-			System.out.println("Invalid port");
-		}
-		if(port == -1){
+		if (port == -1) {
 			return;
 		}
 		client.connect("127.0.0.1", port);
 		try {
-			//if start is private, it's valid here since this main is part of the class
 			client.start();
 		} catch (IOException e) {
 			e.printStackTrace();
